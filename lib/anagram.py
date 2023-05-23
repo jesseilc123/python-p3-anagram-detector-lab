02f497ae5d1608a880228625669f10ef17ad142f
Jesse Ilc <jesseilc123@gmail.com>
2023-05-22 21:08:18 -0500

    Lab complete

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,12 @@
-# your code goes here!
+class Anagram:
+    def __init__(self, word):
+        if word in word:
+            self.word = word
+
+    def match(self, list):
+        match_list = []
+        for i in list:
+            if sorted(self.word) == sorted(i):
+                match_list.append(i)
+        return match_list
+                
